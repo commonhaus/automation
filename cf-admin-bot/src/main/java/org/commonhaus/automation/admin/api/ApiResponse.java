60bf55cf0486ac8480ed1ecf8a722006e7d9d978
Erin Schnabel <erinschnabel@gmail.com>
2024-06-15 21:36:34 -0400

    🎨 remove unused websocket impl

--- cf-admin-bot/src/main/java/org/commonhaus/automation/admin/api/ApiResponse.java
+++ cf-admin-bot/src/main/java/org/commonhaus/automation/admin/api/ApiResponse.java
@@ -3,16 +3,11 @@ package org.commonhaus.automation.admin.api;
 import java.util.HashMap;
 import java.util.Map;
 
-import jakarta.websocket.DecodeException;
-import jakarta.websocket.Decoder;
-import jakarta.websocket.EncodeException;
-import jakarta.websocket.Encoder;
 import jakarta.ws.rs.core.Response;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.quarkus.arc.Arc;
@@ -94,32 +89,5 @@ public class ApiResponse {
                 : Response.status(this.status())
                         .entity(this)
                         .build();
-    }
-
-    public static class MessageEncoder implements Encoder.Text<ApiResponse> {
-        @Override
-        public String encode(ApiResponse object) throws EncodeException {
-            try {
-                return getMapper().writeValueAsString(object);
-            } catch (JsonProcessingException e) {
-                throw new EncodeException(object, e.toString());
-            }
-        }
-    }
-
-    public static class MessageDecoder implements Decoder.Text<ApiResponse> {
-        @Override
-        public ApiResponse decode(String s) throws DecodeException {
-            try {
-                return getMapper().readValue(s, ApiResponse.class);
-            } catch (JsonProcessingException e) {
-                throw new DecodeException(s, e.toString());
-            }
-        }
-
-        @Override
-        public boolean willDecode(String msg) {
-            return true;
-        }
     }
 }
